50ba15e6203698ba6fa212eee0a01bd71798dd8b
returnaction <obergannv@gmail.com>
2025-01-16 19:02:32 +0300

    Add method printThreeWords

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        printThreeWords();
+    }
 
+    // Task 1
+    private static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 }

8882c37510d29f670c4132ee4e1733613888c986
returnaction <obergannv@gmail.com>
2025-01-16 19:06:07 +0300

    Add method checkSumSign

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        // Task 1
         printThreeWords();
+        // Task 2
+        checkSumSign();
     }
 
     // Task 1
@@ -8,5 +11,17 @@ public class Main {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    // Task 2
+    private static void checkSumSign(){
+        int a = 5;
+        int b = 10;
+
+        if( a + b > 0){
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }

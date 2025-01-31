8718d1a3c909f3b24e6ae2eda12fca1465988497
returnaction <obergannv@gmail.com>
2025-01-31 07:09:52 +0300

    Task 2 is completed

--- Task2/Main.java
+++ Task2/Main.java
@@ -2,6 +2,30 @@ package Task2;
 
 public class Main {
     public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.add(80296064176L, "Obergan");
+        phoneBook.add(15612909391L, "Obergan" );
+        phoneBook.add(80291342097L, "Mama");
+        phoneBook.add(80296209391L, "Papa");
+
+        // Выводим всех номера у кого фамилия obergan
+        phoneBook.get("obergan");
+
+        // Выводим всех номера у кого фамилия mama
+        phoneBook.get("mama");
+
+        // поиск несуществующей фамилии
+        phoneBook.get("Dedushka");
+
+        // вывести все номера отсортированные по номеру телефона
+        phoneBook.printAllPhonesInNumberOrder();
+
+        // Вывести все номера отсортированные по имени
+        phoneBook.printAllPhonesInNameOrder();
+
+        // так же сделал перегрузку метода get по телефону.
+        phoneBook.get(80296209391L);
 
     }
 }
--- Task2/PhoneBook.java
+++ Task2/PhoneBook.java
@@ -1,22 +1,62 @@
 package Task2;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class PhoneBook {
-    Map<Long, String> phoneBook;
+    private Map<Long, String> phoneBook;
 
     public PhoneBook() {
-        phoneBook = new HashMap<>();
+        phoneBook = new TreeMap<>();
     }
 
     public void add(long phone, String name){
+        if(name.trim().isEmpty()){
+            System.out.println("Имя не может быть пустым!");
+            return;
+        }
         if(phoneBook.containsKey(phone)){
             System.out.println("Справочник уже содержит номер : " + phone);
             return;
         }
-        phoneBook.put(phone, name);
-        System.out.println(phone + " - " + name + " успешно добавлен");
+        phoneBook.put(phone, name.trim());
+        System.out.println("Добавлен: " + phone + " - " + name);
+    }
+
+    public void get(String name){
+        boolean flag = false;
+        for(Map.Entry<Long, String> entry : phoneBook.entrySet()){
+            if(entry.getValue().equalsIgnoreCase(name.trim())){
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+                flag = true;
+            }
+        }
+        if(!flag){
+            System.out.println("В справочнике нету записи с именем " + name);
+        }
+    }
+
+    public void get(long phone){
+        String name = phoneBook.get(phone);
+        if(name != null){
+            System.out.println(phone + " - " + name);
+        } else {
+            System.out.println("Справочник не содержит номера " + phone);
+        }
+    }
+
+    public void printAllPhonesInNumberOrder(){
+        System.out.println("Все записи записной книжки отсортированной по номеру телефона:");
+        for(Map.Entry<Long, String> entry : phoneBook.entrySet()){
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+
+    public void printAllPhonesInNameOrder(){
+        System.out.println("Все записи отсортированные по фамилии:");
+        phoneBook.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(String.CASE_INSENSITIVE_ORDER))
+                .forEach(phone -> System.out.println(phone.getValue() + " - " + phone.getKey()));
     }
 
 

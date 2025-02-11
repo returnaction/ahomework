03b4b73d210455a1a735f06a3a61f48dfa3a25e1
returnaction <obergannv@gmail.com>
2025-02-11 22:22:20 +0300

    Добавил порядок выполнения

--- src/test/java/nikitatesting/TestClass.java
+++ src/test/java/nikitatesting/TestClass.java
@@ -1,15 +1,16 @@
 package nikitatesting;
 
 import org.example.Main;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestClass {
 
     @Test
     @DisplayName("Тестируем позитивные числа факториала")
+    @Order(1)
     public void testFactorial(){
         assertEquals(120, Main.factorial(5));
         assertEquals(720, Main.factorial(6));
@@ -17,12 +18,14 @@ public class TestClass {
 
     @DisplayName("Тестируем факториал 0")
     @Test
+    @Order(2)
     public void testFactorial0Equals1(){
         assertEquals(1, Main.factorial(0));
     }
 
     @DisplayName("Тестируем факториал 1")
     @Test
+    @Order(3)
     public void testFactorial1Equals1(){
         assertEquals(1, Main.factorial(1));
     }

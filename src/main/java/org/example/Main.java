3efc89e5f6c0c058d3bd8c0598632b8ada765b7a
returnaction <obergannv@gmail.com>
2025-02-12 06:26:27 +0300

    Init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,42 +1,14 @@
 package org.example;
 
-import java.util.Scanner;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
 
-        factorialProgram();
-    }
-
-    public static long factorial(int n) {
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-
-        return result;
-    }
-
-    public static void factorialProgram() {
-        Scanner sc = new Scanner(System.in);
-        int input;
-        while (true) {
-            try {
-                System.out.print("Введите число: ");
-                input = Integer.parseInt(sc.next());
-                if (input < 0) {
-                    System.out.println("Факториал нельзя найти для отрицательных чисел");
-                } else {
-                    break;
-                }
-            } catch (Exception e) {
-                System.out.println("Введите целое число");
-            }
-        }
-        sc.close();
-
-        long factorialResult = factorial(input);
-        System.out.println("Факториал числа  " + input + " = " + factorialResult);
-
+        // Если планируем запускать тесты локально, и мы  знаем какие тесты и на чем мы будем запускать
+        System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
     }
 }
--- src/test/java/nikitatesting/TestClass.java
+++ src/test/java/nikitatesting/TestClass.java
@@ -1,26 +1,4 @@
 package nikitatesting;
 
-
-import org.example.Main;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 public class TestClass {
-
-    @Test(description = "Проверяет обычные числа факториала", priority = 1)
-    public void testFactorial(){
-        Assert.assertEquals(Main.factorial(3), 6);
-        Assert.assertEquals(Main.factorial(10), 3_628_800);
-    }
-
-    @Test(description = "Проверяет что факториал 0 равен 1", priority = 2)
-    public void testFactorial0Equals1(){
-        Assert.assertEquals(Main.factorial(0), 1);
-    }
-
-    @Test(description = "Проверяет что факториал 0 равен 1", priority = 3)
-    public void testFactorial1Equals2(){
-        Assert.assertEquals(Main.factorial(1), 1);
-    }
-
 }

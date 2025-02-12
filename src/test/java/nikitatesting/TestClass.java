d9501fd54ade289f38fd441de907e9fff00f0215
returnaction <obergannv@gmail.com>
2025-02-12 06:40:02 +0300

    add first Selenium methods

--- src/test/java/nikitatesting/TestClass.java
+++ src/test/java/nikitatesting/TestClass.java
@@ -1,4 +1,30 @@
 package nikitatesting;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestClass {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void openURL() throws InterruptedException {
+        driver.get("https://mail.ru/");
+        Thread.sleep(10000);
+    }
+
 }
